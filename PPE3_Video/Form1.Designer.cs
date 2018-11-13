adf4a81365f6f80262b03e27eb1d23729934c991
Lemasson Ulric <ulric.lemasson@gmail.com>
2018-11-13 16:21:15 +0100

    ajout des menus

--- PPE3_Video/Form1.Designer.cs
+++ PPE3_Video/Form1.Designer.cs
@@ -28,21 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rechercheToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.composantsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.parGenreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.parSupportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.parClientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.parRéférenceEmpruntToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.parDateAbonnementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.validationDunCompteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vérificationDunCompteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fermetureDunCompteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajoutDunCompteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fichierToolStripMenuItem,
+            this.rechercheToolStripMenuItem,
+            this.compteToolStripMenuItem,
+            this.composantsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fichierToolStripMenuItem
+            // 
+            this.fichierToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitterToolStripMenuItem});
+            this.fichierToolStripMenuItem.Name = "fichierToolStripMenuItem";
+            this.fichierToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.fichierToolStripMenuItem.Text = "Fichier";
+            // 
+            // rechercheToolStripMenuItem
+            // 
+            this.rechercheToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.parGenreToolStripMenuItem,
+            this.parSupportToolStripMenuItem,
+            this.parClientToolStripMenuItem,
+            this.parRéférenceEmpruntToolStripMenuItem,
+            this.parDateAbonnementToolStripMenuItem});
+            this.rechercheToolStripMenuItem.Name = "rechercheToolStripMenuItem";
+            this.rechercheToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.rechercheToolStripMenuItem.Text = "Recherches";
+            // 
+            // compteToolStripMenuItem
+            // 
+            this.compteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.validationDunCompteToolStripMenuItem,
+            this.vérificationDunCompteToolStripMenuItem,
+            this.fermetureDunCompteToolStripMenuItem,
+            this.ajoutDunCompteToolStripMenuItem});
+            this.compteToolStripMenuItem.Name = "compteToolStripMenuItem";
+            this.compteToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.compteToolStripMenuItem.Text = "Comptes";
+            // 
+            // composantsToolStripMenuItem
+            // 
+            this.composantsToolStripMenuItem.Name = "composantsToolStripMenuItem";
+            this.composantsToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.composantsToolStripMenuItem.Text = "Composants";
+            // 
+            // quitterToolStripMenuItem
+            // 
+            this.quitterToolStripMenuItem.Name = "quitterToolStripMenuItem";
+            this.quitterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quitterToolStripMenuItem.Text = "Quitter";
+            // 
+            // parGenreToolStripMenuItem
+            // 
+            this.parGenreToolStripMenuItem.Name = "parGenreToolStripMenuItem";
+            this.parGenreToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.parGenreToolStripMenuItem.Text = "Par Genre";
+            // 
+            // parSupportToolStripMenuItem
+            // 
+            this.parSupportToolStripMenuItem.Name = "parSupportToolStripMenuItem";
+            this.parSupportToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.parSupportToolStripMenuItem.Text = "Par Support";
+            // 
+            // parClientToolStripMenuItem
+            // 
+            this.parClientToolStripMenuItem.Name = "parClientToolStripMenuItem";
+            this.parClientToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.parClientToolStripMenuItem.Text = "Par Client";
+            // 
+            // parRéférenceEmpruntToolStripMenuItem
+            // 
+            this.parRéférenceEmpruntToolStripMenuItem.Name = "parRéférenceEmpruntToolStripMenuItem";
+            this.parRéférenceEmpruntToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.parRéférenceEmpruntToolStripMenuItem.Text = "Par Référence Emprunt";
+            // 
+            // parDateAbonnementToolStripMenuItem
+            // 
+            this.parDateAbonnementToolStripMenuItem.Name = "parDateAbonnementToolStripMenuItem";
+            this.parDateAbonnementToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.parDateAbonnementToolStripMenuItem.Text = "Par date Abonnement";
+            // 
+            // validationDunCompteToolStripMenuItem
+            // 
+            this.validationDunCompteToolStripMenuItem.Name = "validationDunCompteToolStripMenuItem";
+            this.validationDunCompteToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.validationDunCompteToolStripMenuItem.Text = "Validation d\'un compte";
+            // 
+            // vérificationDunCompteToolStripMenuItem
+            // 
+            this.vérificationDunCompteToolStripMenuItem.Name = "vérificationDunCompteToolStripMenuItem";
+            this.vérificationDunCompteToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.vérificationDunCompteToolStripMenuItem.Text = "Vérification d\'un compte";
+            // 
+            // fermetureDunCompteToolStripMenuItem
+            // 
+            this.fermetureDunCompteToolStripMenuItem.Name = "fermetureDunCompteToolStripMenuItem";
+            this.fermetureDunCompteToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.fermetureDunCompteToolStripMenuItem.Text = "Fermeture d\'un compte";
+            // 
+            // ajoutDunCompteToolStripMenuItem
+            // 
+            this.ajoutDunCompteToolStripMenuItem.Name = "ajoutDunCompteToolStripMenuItem";
+            this.ajoutDunCompteToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.ajoutDunCompteToolStripMenuItem.Text = "Ajout d\'un compte";
             // 
             // FromAcceuil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FromAcceuil";
             this.Text = "GestionVideo";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rechercheToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem composantsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem parGenreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem parSupportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem parClientToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem parRéférenceEmpruntToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem parDateAbonnementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem validationDunCompteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vérificationDunCompteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fermetureDunCompteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajoutDunCompteToolStripMenuItem;
     }
 }
 
