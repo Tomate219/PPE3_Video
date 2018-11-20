82edb1ad2f8ed09c1131c3d9b82af8b96d6a18a7
Tomate219 <near219@gmail.com>
2018-11-20 14:25:11 +0100

    Ajout du data set

--- PPE3_Video/FormAjouterGenre.Designer.cs
+++ PPE3_Video/FormAjouterGenre.Designer.cs
@@ -28,8 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btValider = new System.Windows.Forms.Button();
             this.tbNom = new System.Windows.Forms.TextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.videoPPE3Groupe4DataSet = new PPE3_Video.VideoPPE3Groupe4DataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoPPE3Groupe4DataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // btValider
@@ -50,6 +55,16 @@
             this.tbNom.TabIndex = 1;
             this.tbNom.Text = "Saisissez le nom du genre";
             // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataSource = this.videoPPE3Groupe4DataSet;
+            this.bindingSource1.Position = 0;
+            // 
+            // videoPPE3Groupe4DataSet
+            // 
+            this.videoPPE3Groupe4DataSet.DataSetName = "VideoPPE3Groupe4DataSet";
+            this.videoPPE3Groupe4DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
             // FormAjouterGenre
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -60,6 +75,8 @@
             this.Name = "FormAjouterGenre";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Ajouter un Genre";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoPPE3Groupe4DataSet)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,5 +86,7 @@
 
         private System.Windows.Forms.Button btValider;
         private System.Windows.Forms.TextBox tbNom;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private VideoPPE3Groupe4DataSet videoPPE3Groupe4DataSet;
     }
 }
